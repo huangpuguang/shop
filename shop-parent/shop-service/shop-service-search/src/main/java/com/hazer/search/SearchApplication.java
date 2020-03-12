72bd256c8721315aa52a7936d0c81331ef8e664e
Gyo Howar <1187147080@qq.com>
2020-03-12 23:42:10 +0800

    代码生成

---
+++ shop-parent/shop-service/shop-service-search/src/main/java/com/hazer/search/SearchApplication.java
@@ -0,0 +1,27 @@
+package com.hazer.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @ClassName: SearchApplication
+ * @description: 类的说明
+ * @author: Hazer
+ * @create: 2020-03-12 22:53
+ **/
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@EnableEurekaClient
+public class SearchApplication {
+
+    public static void main(String[] args) {
+        /**
+         * Springboot整合Elasticsearch 在项目启动前设置一下的属性，防止报错
+         * 解决netty冲突后初始化client时还会抛出异常
+         * availableProcessors is already set to [12], rejecting [12]
+         ***/
+        System.setProperty("es.set.netty.runtime.available.processors", "false");
+        SpringApplication.run(SearchApplication.class,args);
+    }
+}
